86c347cb3cd538ebe78a561c80ebd6a7507ccdec
David Jegat <david.jegat@knplabs.com>
2021-04-16 15:50:23 +0200

    Introduce authorization

--- src/Controller/Admin/PizzaController.php
+++ src/Controller/Admin/PizzaController.php
@@ -7,11 +7,13 @@ use App\Form\AdminPizzaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 
 /**
  * @Route("/admin/pizza")
+ * @IsGranted("ROLE_ADMIN")
  */
 class PizzaController extends AbstractController
 {
