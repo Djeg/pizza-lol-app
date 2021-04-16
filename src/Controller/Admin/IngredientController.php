83c03a7d330d840e3a3708aaf16c52210689a35e
David Jegat <david.jegat@knplabs.com>
2021-04-16 12:13:01 +0200

    Create ingredient

--- src/Controller/Admin/IngredientController.php
+++ src/Controller/Admin/IngredientController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Ingredient;
+use App\Form\AdminIngredientType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,6 +24,32 @@ class IngredientController extends AbstractController
 
         return $this->render('admin/ingredient/list.html.twig', [
             'ingredients' => $ingredients,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/ingredients/create", name="app_admin_ingredient_create")
+     */
+    public function create(Request $request): Response
+    {
+        $form = $this->createForm(AdminIngredientType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_admin_ingredient_list');
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('admin/ingredient/create.html.twig', [
+            'formView' => $formView,
         ]);
     }
 }
