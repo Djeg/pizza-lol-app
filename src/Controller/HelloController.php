c25cabf475dad3ed62748b2f752a0efa0db25735
David Jegat <david.jegat@gmail.com>
2021-10-13 15:57:00 +0200

    Ajout exo calcul

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -7,6 +7,20 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * 1. Créer un controller : "CalculController"
+ * 
+ * 2. Ajouter un méthode "add" qui accépte
+ *    2 paramètre de type int, qui les additiones,
+ *    et qui retourne une réponse: "Resultat : {le resultat}"
+ * 
+ * 3. Ajouter une méthode "multiply" qui fait la
+ *    multiplication
+ * 
+ * 4. Ajouter un méthode "subtract" qui fais la
+ *   soustraction
+ */
+
 class HelloController
 {
     /**
@@ -15,5 +29,21 @@ class HelloController
     public function hello(): Response
     {
         return new Response('Bonjour tout le monde');
+    }
+
+    /**
+     * @Route("/bonjour/{param1}", name="app_hello_bonjour")
+     */
+    public function bonjour(string $param1): Response
+    {
+        return new Response('Bonjour ' . $param1);
+    }
+
+    /**
+     * @Route("/hello2/{param2}", name="app_hello_hello2")
+     */
+    public function hello2(int $param2 = 23): Response
+    {
+        return new Response('cocoucou tout le monde : ' . $param2);
     }
 }
