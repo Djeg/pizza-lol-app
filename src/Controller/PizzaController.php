cd228dac41a7d7d8f7c0a2fc3a195c7852b86f71
David Jegat <david.jegat@gmail.com>
2021-10-15 16:30:21 +0200

    Creation de la pizza

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Pizza;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PizzaController extends AbstractController
 {
@@ -21,8 +23,34 @@ class PizzaController extends AbstractController
     /**
      * @Route("/nouvelle-pizza", name="app_pizza_create")
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
+        $method = $request->getMethod();
+
+        if ('POST' === $method) {
+            // On remplie notre entité
+            $pizza = new Pizza();
+            $pizza->setName($request->request->get('name'));
+            $pizza->setDescription($request->request->get('description'));
+            $pizza->setPrice((float)$request->request->get('price'));
+            $pizza->setImage($request->request->get('image'));
+
+            // On récupére l'entity manager
+            $manager = $this->getDoctrine()->getManager();
+
+            // On persiste notre pizza. On enregistre la pizza
+            // dans notre manager
+            $manager->persist($pizza);
+
+            // Transmet tout à la base de données
+            $manager->flush();
+
+            // Rediriger vers display
+            return $this->redirectToRoute('app_pizza_display', [
+                'id' => $pizza->getId(),
+            ]);
+        }
+
         return $this->render('pizza/create.html.twig');
     }
 
@@ -31,6 +59,12 @@ class PizzaController extends AbstractController
      */
     public function display(int $id): Response
     {
-        return $this->render('pizza/display.html.twig');
+        $repository = $this->getDoctrine()->getRepository(Pizza::class);
+
+        $pizza = $repository->find($id);
+
+        return $this->render('pizza/display.html.twig', [
+            'pizza' => $pizza,
+        ]);
     }
 }
