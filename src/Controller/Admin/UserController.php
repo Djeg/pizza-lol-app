ad8f17780d93430b30e4895eea1ecc5056e28396
David Jegat <david.jegat@gmail.com>
2021-10-25 13:11:10 +0200

    Ajout du CRUD utilisateur

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Form\Admin\UserType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Encoder\PasswordHasherEncoder;
+
+class UserController extends AbstractController
+{
+    #[Route('/admin/users', name: 'app_admin_user_index')]
+    public function index(): Response
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        return $this->render('admin/user/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/admin/users/nouveau', name: 'app_admin_user_create')]
+    public function create(Request $request, UserPasswordHasherInterface $hasher): Response
+    {
+        $form = $this->createForm(UserType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $user->setPassword($hasher->hashPassword($user, $user->getPassword()));
+
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($user);
+
+            $manager->flush();
+
+            return $this->redirectToRoute('app_admin_user_index', [
+                'success' => "L'utilisateur {$user->getEmail()} à bien été créé",
+            ]);
+        }
+
+        return $this->render('admin/user/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/users/{id}', name: 'app_admin_user_update')]
+    public function update(User $user, Request $request, UserPasswordHasherInterface $hasher): Response
+    {
+        $form = $this->createForm(UserType::class, $user, [
+            'modify_mode' => true,
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            if ($form->get('password')->getData()) {
+                $user->setPassword($hasher->hashPassword($user, $user->getPassword()));
+            }
+
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($user);
+
+            $manager->flush();
+
+            return $this->redirectToRoute('app_admin_user_index', [
+                'success' => "L'utilisateur {$user->getEmail()} à bien été mis à jour",
+            ]);
+        }
+
+        return $this->render('admin/user/update.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/users/{id}/supprimer', name: 'app_admin_user_delete')]
+    public function delete(User $user, Request $request): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        $manager->remove($user);
+        $manager->flush();
+
+        return $this->redirectToRoute('app_admin_user_index', [
+            'success' => "L'utilisateur {$user->getEmail()} à bien été supprimé",
+        ]);
+    }
+}
